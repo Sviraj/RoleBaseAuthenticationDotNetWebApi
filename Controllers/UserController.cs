b1c038692bf875c331d0d14efd783ec5ec406b80
sachinviraj <74185781+Sviraj@users.noreply.github.com>
2024-07-22 19:03:27 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocationTracker.Controllers
+{
+    [Authorize(Roles = "User")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("You have access the User Controller");
+        }
+    }
+}
